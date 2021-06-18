2000411f18ff80c562ec3d9e3dba68dc527ccbd8
Paulina Korpak <korpak.paulina@gmail.com>
2021-06-18 17:28:49 +0100

    Handle item adding

--- src/features/todo/components/Form/index.tsx
+++ src/features/todo/components/Form/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { KeyboardEvent } from 'react';
+import { useAppDispatch } from '../../../../app/hooks';
+import { addTodo } from '../../todoSlice';
 
 function Form() {
+  const dispatch = useAppDispatch();
+
+  const handleChange = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && e.currentTarget.value !== '') {
+      const todo = {
+        id: Date.now(),
+        title: e.currentTarget.value,
+        completed: false,
+      };
+
+      dispatch(addTodo(todo));
+
+      e.currentTarget.value = '';
+    }
+  };
+
   return (
     <div className="w-100">
       <input
+        onKeyDown={handleChange}
         type="text"
         placeholder="add new todo"
         className="form-control form-control-color-secondary p-4"
--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import FilterType from './filterType';
 
 export interface Todo {
@@ -20,7 +20,13 @@ const initialState: TodoState = {
 export const todoSlice = createSlice({
   name: 'todo',
   initialState,
-  reducers: {},
+  reducers: {
+    addTodo: (state, action: PayloadAction<Todo>) => {
+      state.todos = [...state.todos, action.payload];
+    },
+  },
 });
+
+export const { addTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
